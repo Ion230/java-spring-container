0aaf5b62abd66763bfe7a3eb6c864bdedfd01f52
Ion Botnariuc <ionelbot23@gmail.com>
2025-02-22 10:52:30 +0200

    Intitial commit.

---
+++ src/main/java/pixel/academy/spring_core_container/SpringCoreContainerApplication.java
@@ -0,0 +1,13 @@
+package pixel.academy.spring_core_container;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCoreContainerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCoreContainerApplication.class, args);
+	}
+
+}
